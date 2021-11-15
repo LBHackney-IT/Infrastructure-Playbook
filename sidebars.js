f018abaef0f7dde91c8b60a34525757ec4500bbe
LBHMkeyworth <matthew.keyworth@hackney.gov.uk>
2021-11-15 08:55:27 +0000

    Menu change

--- sidebars.js
+++ sidebars.js
@@ -8,7 +8,7 @@ module.exports = {
     {
       type: 'category',
       label: 'Info',
-      items: ['support_processes','control_tower','sso_sync','permissions_management','account_provisioning','hub'],
+      items: ['services_we_provide','support_processes','control_tower','sso_sync','permissions_management','account_provisioning','hub'],
     },
     {
       type: 'category',
