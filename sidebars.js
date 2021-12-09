e362db59f39d92327dccd71e0ab176021d946cdb
Anna C M <62144201+acanbym@users.noreply.github.com>
2021-12-09 15:27:37 +0000

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -8,7 +8,7 @@ module.exports = {
     {
       type: 'category',
       label: 'Info',
-      items: ['services_we_provide','support_processes','control_tower','sso_sync','permissions_management','account_provisioning','hub', 'change_process','backups','aws_cli', 'ways_of_working'],
+      items: ['services_we_provide','support_processes','control_tower','sso_sync','permissions_management','account_provisioning','hub', 'change_process','backups','aws_cli', 'ways_of_working', 'supported_and_unsupported'],
     },
     {
       type: 'category',
